d0a9badac8dce4a2073b12424833d845a3d227d9
tsangkafu <47711510+tsangkafu@users.noreply.github.com>
2022-03-15 15:57:14 -0600

    Add files via upload

---
+++ walmart.js
@@ -0,0 +1,25 @@
+// remove the unavilable items
+document.querySelector(`div[data-automation="unavailable-items"]`).parentNode.remove();
+// number of total items
+let numOfItem = document.querySelectorAll(`div[data-automation="item-card"] div > a`).length;
+
+// number of substitutions
+let numOfSub = document.querySelectorAll(`div[data-automation="substitution-card"]`).length;
+// remove substitution cards
+document.querySelectorAll(`div[data-automation="substitution-card"]`).forEach(node => node.remove());
+
+let name = [];
+let price = [];
+
+for(let i = 0, j = 0; i < numOfItem; i++){
+    // for substitution
+    if (i >= numOfItem - numOfSub){
+    	name.push(document.querySelectorAll(`p[data-automation="item-name-${j}"]`)[1].textContent);
+	    j++;
+    } else {
+    	name.push(document.querySelector(`p[data-automation="item-name-${i}"]`).textContent);
+    }
+    price.push(document.querySelectorAll(`div[data-automation="item-card"] > div > div:nth-child(2) span > span`)[i].textContent.replace("$", ""));
+}
+
+console.log(name.join("\n") + "\n" + price.join("\n"));
